f45f5449d2f51f7be97525dff2eafd0d158d75b8
Tornike Goshadze <joffarex@gmail.com>
2021-03-21 21:45:21 +0400

    Add path utils for asset loading

---
+++ AcidarX.Core/Utils/PathUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+
+public static class PathUtils
+{
+    public static string GetFullPath(string path) => Path.Combine(TryGetSolutionDirectoryInfo().FullName, path);
+
+    public static DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
+    {
+        var directory = new DirectoryInfo(
+            currentPath ?? Directory.GetCurrentDirectory());
+        while (directory != null && !directory.GetFiles("*.sln").Any())
+        {
+            directory = directory.Parent;
+        }
+
+        return directory;
+    }
+}
