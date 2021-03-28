0377d2c3797fc6da91887976b56c07c76c2e2293
Tornike Goshadze <joffarex@gmail.com>
2021-03-28 13:00:19 +0400

    Implement type utilities for size

---
+++ AcidarX.Core/Utils/TypeUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection.Emit;
+
+namespace AcidarX.Core.Utils
+{
+    public static class TypeUtils
+    {
+        private static readonly ConcurrentDictionary<Type, int>
+            Cache = new();
+
+        public static int SizeOf<T>() where T : struct => SizeOf(typeof(T?));
+
+        public static int SizeOf<T>(T obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            return SizeOf(obj.GetType());
+        }
+
+        public static int SizeOf(Type t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("t");
+            }
+
+            return Cache.GetOrAdd(t, t2 =>
+            {
+                var dm = new DynamicMethod("$", typeof(int), Type.EmptyTypes);
+                ILGenerator il = dm.GetILGenerator();
+                il.Emit(OpCodes.Sizeof, t2);
+                il.Emit(OpCodes.Ret);
+
+                Func<int> func = (Func<int>) dm.CreateDelegate(typeof(Func<int>));
+                return func();
+            });
+        }
+    }
+}
