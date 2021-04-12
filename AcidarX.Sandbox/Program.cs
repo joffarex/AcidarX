f1a0181a77629bde20b3a9b4ccc820e6874e0fe9
Tornike Goshadze <joffarex@gmail.com>
2021-04-12 20:39:15 +0400

    Update instrumentation paths

--- AcidarX.Sandbox/Program.cs
+++ AcidarX.Sandbox/Program.cs
@@ -3,14 +3,14 @@ using AcidarX.Core.Profiling;
 using AcidarX.Core.Windowing;
 using AcidarX.Sandbox;
 
-Instrumentation.Instance.BeginSession("Startup", "profile-Startup.json");
+Instrumentation.Instance.BeginSession("Startup", "Sandbox/profile-Startup.json");
 var windowOptions = AXWindowOptions.CreateDefault();
 var application = AXHostApplication.Create(windowOptions);
 Instrumentation.Instance.EndSession();
-Instrumentation.Instance.BeginSession("Runtime", "profile-Runtime.json");
+Instrumentation.Instance.BeginSession("Runtime", "Sandbox/profile-Runtime.json");
 application.PushLayer<Sandbox2DLayer>();
 application.Run();
 Instrumentation.Instance.EndSession();
-Instrumentation.Instance.BeginSession("Shutdown", "profile-Shutdown.json");
+Instrumentation.Instance.BeginSession("Shutdown", "Sandbox/profile-Shutdown.json");
 application.Dispose();
 Instrumentation.Instance.EndSession();
--- EditarX/Program.cs
+++ EditarX/Program.cs
@@ -3,14 +3,14 @@ using AcidarX.Core.Profiling;
 using AcidarX.Core.Windowing;
 using EditarX;
 
-Instrumentation.Instance.BeginSession("Startup", "profile-Startup.json");
+Instrumentation.Instance.BeginSession("Startup", "EditarX/profile-Startup.json");
 var windowOptions = new AXWindowOptions() {Title = "EditarX", Width = 1920, Height = 1080, VSync = true};
 var application = AXHostApplication.Create(windowOptions);
 Instrumentation.Instance.EndSession();
-Instrumentation.Instance.BeginSession("Runtime", "profile-Runtime.json");
+Instrumentation.Instance.BeginSession("Runtime", "EditarX/profile-Runtime.json");
 application.PushLayer<EditorLayer>();
 application.Run();
 Instrumentation.Instance.EndSession();
-Instrumentation.Instance.BeginSession("Shutdown", "profile-Shutdown.json");
+Instrumentation.Instance.BeginSession("Shutdown", "EditarX/profile-Shutdown.json");
 application.Dispose();
 Instrumentation.Instance.EndSession();
